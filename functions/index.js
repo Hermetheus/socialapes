46b6d0d26217b7d708c8d0d2208806173825ca51
Allan Olson <allanmolson@gmail.com>
2019-07-07 22:42:42 -0500

    ImageUpload

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 const FBAuth = require("./util/fbAuth");
 
 //users Routes - handlers/user
@@ -11,5 +11,6 @@ app.post("/login", login);
 // Scream Routes - handlers/screams
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
+app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.https.onRequest(app);
